b70b69c07ff9fa72ed3e2dfe70742bc74dd0afd0
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-05-29 14:01:50 -0500

    Update changes to input element component

--- src/app/input-element/input-element.component.ts
+++ src/app/input-element/input-element.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-element',
   templateUrl: './input-element.component.html',
   styleUrls: ['./input-element.component.scss']
 })
+
 export class InputElementComponent implements OnInit {
+
+  @Input() dummyInputData:any;
+
+  inputNumberControl = new FormControl('', Validators.required);
+  inputTextControl;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    
+    //This condition will check for different error message and then create form control instance
+    if('emailErrorMsg' in this.dummyInputData.input){
+      this.inputTextControl = new FormControl('',[Validators.required, Validators.email]);
+    }
+    else{
+      this.inputTextControl = new FormControl('',Validators.required);
+    }
   }
 
 }
