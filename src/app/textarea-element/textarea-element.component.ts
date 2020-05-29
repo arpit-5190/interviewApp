704764ad9c1bd0586c1ac2123ed590a87a98eb84
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-05-29 14:25:28 -0500

    update changes to textarea element component

--- src/app/textarea-element/textarea-element.component.ts
+++ src/app/textarea-element/textarea-element.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-textarea-element',
   templateUrl: './textarea-element.component.html',
   styleUrls: ['./textarea-element.component.scss']
 })
+
 export class TextareaElementComponent implements OnInit {
 
+  //Define Input variable which is used to get data from parent app component to child component
+  @Input() dummyTextAreaData:any;
+
+  //Define a form control instance which will be used for validation
+  textareaControl = new FormControl('', Validators.required);
   constructor() { }
 
   ngOnInit() {
