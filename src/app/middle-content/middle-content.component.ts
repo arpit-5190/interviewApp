a802723a7c3322750927d4667a3ace528371cbd6
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-05-29 11:51:11 -0500

    Update files to middle content component

--- src/app/middle-content/middle-content.component.ts
+++ src/app/middle-content/middle-content.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { RestApiService } from './../rest-api.service';
+import { Component, OnInit, Input } from '@angular/core';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-middle-content',
   templateUrl: './middle-content.component.html',
-  styleUrls: ['./middle-content.component.scss']
+  styleUrls: ['./middle-content.component.scss'],
+  providers: [
+    {provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}}
+  ]
 })
 export class MiddleContentComponent implements OnInit {
 
-  constructor() { }
+  //Define Input property for get data from parent App component
+  @Input() dummyCategoryData:any;
 
-  ngOnInit() {
+  //Define public property which just store data for intercept.
+  public dummyVal = this.dummyCategoryData;
+  public deviceWidth: number;
+  public elementName:any;
+
+  isLinear = false;
+  verticalFormGroup: FormGroup;
+
+  reachTop = (event:any) => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop =0;
+  }
+
+  constructor(private _formBuilder: FormBuilder, private httpService:RestApiService) { }
+
+  ngOnInit():void {
+    this.verticalFormGroup = this._formBuilder.group({
+      verticalCtrl: ['', Validators.required]
+    });
+
+    this.deviceWidth = window.innerWidth;
   }
 
 }
