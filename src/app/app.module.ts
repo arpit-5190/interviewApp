f59b20803b4d5601ea182d888a6e5e62d8b14508
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-05-29 12:06:13 -0500

    add module packages to app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//import animation module
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//import reactive forms module
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Import components
 import { BodyContentComponent } from './body-content/body-content.component';
 import { AutocompleteElementComponent } from './autocomplete-element/autocomplete-element.component';
 import { CheckboxElementComponent } from './checkbox-element/checkbox-element.component';
@@ -14,6 +21,25 @@ import { RadioElementComponent } from './radio-element/radio-element.component';
 import { SelectElementComponent } from './select-element/select-element.component';
 import { TextareaElementComponent } from './textarea-element/textarea-element.component';
 import { SliderElmentComponent } from './slider-elment/slider-elment.component';
+
+//Import Angular Material Element Modules
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatRippleModule } from '@angular/material/core';
+
+//Import httpclient module
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,7 +58,24 @@ import { SliderElmentComponent } from './slider-elment/slider-elment.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatStepperModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatRippleModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
