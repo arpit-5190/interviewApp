0d441c17850b3551709aa5526dcd14f19bc5b721
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-05-29 13:45:18 -0500

    update changes to datepicker element component

--- src/app/datepicker-element/datepicker-element.component.ts
+++ src/app/datepicker-element/datepicker-element.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-datepicker-element',
   templateUrl: './datepicker-element.component.html',
   styleUrls: ['./datepicker-element.component.scss']
 })
+
 export class DatepickerElementComponent implements OnInit {
 
+  //Define an Input variable which is used to navigate data from parent app component to child component
+  @Input() dummyDatapickerData:any;
   constructor() { }
 
   ngOnInit() {
