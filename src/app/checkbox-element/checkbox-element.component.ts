d73f40a3d9983224691750101b0cabb0dfdec29c
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-05-29 13:39:00 -0500

    update changes to checkbox element component

--- src/app/checkbox-element/checkbox-element.component.ts
+++ src/app/checkbox-element/checkbox-element.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox-element',
   templateUrl: './checkbox-element.component.html',
   styleUrls: ['./checkbox-element.component.scss']
 })
+
 export class CheckboxElementComponent implements OnInit {
 
+  //Define Input variable which is used to get data from parent App component to child component
+  @Input() dummyCheckboxData:any;
+  
   constructor() { }
 
   ngOnInit() {
