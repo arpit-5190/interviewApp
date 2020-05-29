05d1fd3278049e270b912f7c6ed7c9fd66776fb3
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-05-29 14:08:38 -0500

    update radio element component changes

--- src/app/radio-element/radio-element.component.ts
+++ src/app/radio-element/radio-element.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-radio-element',
   templateUrl: './radio-element.component.html',
   styleUrls: ['./radio-element.component.scss']
 })
+
 export class RadioElementComponent implements OnInit {
 
+  //Define Input variable which will communicate from parent app component to child component
+  @Input() dummyRadioData:any;
+  
   constructor() { }
 
   ngOnInit() {
