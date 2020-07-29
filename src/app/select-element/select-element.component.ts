a9ccd2de2ba82b1a90e5bbb0fefb8b556a006374
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-07-29 14:31:54 -0500

    add changes for select dropdown for put operation

--- src/app/select-element/select-element.component.ts
+++ src/app/select-element/select-element.component.ts
@@ -1,5 +1,5 @@
 import { FormControl, Validators } from '@angular/forms';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-select-element',
@@ -11,12 +11,37 @@ export class SelectElementComponent implements OnInit {
   //Define an Input variable which is used to communicate with parent component
   @Input() dummySelectData:any;
 
+  //Define an output variable which is used to pass the data from child to parent component
+  @Output() public onSelectValueChange = new EventEmitter();
+
   //Define formcontrol which will be used for reactive form validation
-  selectControl = new FormControl('', Validators.required);
+  public selectControl:any;
+
+  //Define the property which will be used to pass the data in object format
+  public selectValue:any = {};
+
+  changeClient(data){
+    console.log("dropdown value", data);
+  }
+
+  //Define the event which will be called when dropdown value changes
+  selectChange = (value:any): void => {
+    console.log("Value CHanges for select");
+    this.selectValue["label"] = this.dummySelectData.question.text;
+    this.selectValue["value"] = value;
+    console.log("value changes for select", this.selectValue);
+    this.onSelectValueChange.emit(this.selectValue);
+  }
 
   constructor() { }
 
   ngOnInit() {
+    //Define the question label and answer to the onInit
+    this.selectValue["label"] = this.dummySelectData.question.text;
+    this.selectValue["value"] = this.dummySelectData.question.answer;
+
+    //Define the form control instance for validation
+    this.selectControl = new FormControl(this.selectValue["value"], Validators.required);
   }
 
 }
