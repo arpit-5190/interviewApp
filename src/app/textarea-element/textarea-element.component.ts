3eec1d73c8e12e3fed162e5e534e49f646d11dec
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-07-29 14:42:56 -0500

    textarea element update for put operation

--- src/app/textarea-element/textarea-element.component.ts
+++ src/app/textarea-element/textarea-element.component.ts
@@ -1,5 +1,5 @@
 import { FormControl, Validators } from '@angular/forms';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-textarea-element',
@@ -12,11 +12,32 @@ export class TextareaElementComponent implements OnInit {
   //Define Input variable which is used to get data from parent app component to child component
   @Input() dummyTextAreaData:any;
 
-  //Define a form control instance which will be used for validation
-  textareaControl = new FormControl('', Validators.required);
+  //Define output variable to pass the valuw of the question to parent component
+  @Output() public onTextAreaValueChange = new EventEmitter();
+
+  //Define property to store input value
+  textAreaValue:any = {};
+
+  //Define an event which is called while input text field will change value
+  textAreaChange = (e:any):void => {
+    this.textAreaValue["label"] = this.dummyTextAreaData.question.text;
+    this.textAreaValue["value"] = e;
+    this.onTextAreaValueChange.emit(this.textAreaValue);
+  }
+
+  //Define a form control instance which will be used for control validation
+  public textareaControl:any;
+
   constructor() { }
 
   ngOnInit() {
+    
+    this.textAreaValue["label"] = this.dummyTextAreaData.question.label;
+    this.textAreaValue["value"] = this.dummyTextAreaData.question.answer;
+
+    //Define a form control instance which will use for validation
+    this.textareaControl = new FormControl(this.textAreaValue["value"], Validators.required);
+  
   }
 
 }
