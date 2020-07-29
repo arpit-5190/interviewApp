73e210639a65b970ea5ecefeeed7722760400c62
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-07-29 14:17:59 -0500

    update radio element component for put operation

--- src/app/radio-element/radio-element.component.ts
+++ src/app/radio-element/radio-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-radio-element',
@@ -8,12 +9,36 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class RadioElementComponent implements OnInit {
 
-  //Define Input variable which will communicate from parent app component to child component
+  //Define Input variable which wil communicate from parent Appc omponent to child component
   @Input() dummyRadioData:any;
+
+  //Define Output variable which is used to pass the data from child to parent component
+  @Output() public onRadioValueChange = new EventEmitter();
+
+  //Define form control instance whic hwill be used to form validation
+  public radioControl:any;
+
+  //Define the property whihc iwll be used to pass the data in object format
+  public radioValue:any = {};
+
+  //Define the event which will be called when radio value changes
+  radioChange = (value:any):void => {
+    this.radioValue["label"] = this.dummyRadioData.question.text;
+    this.radioValue["value"] = value;
+    this.onRadioValueChange.emit(this.radioValue);
+  }
+
+  public selection:string;
+
   
   constructor() { }
 
   ngOnInit() {
+    //Define the question label and answer to the Oninit
+    this.radioValue["label"] = this.dummyRadioData.question.text;
+    this.radioValue["value"] = this.dummyRadioData.question.answer;
+    this.selection = this.radioValue["value"];
+    this.radioControl = new FormControl(this.radioValue["value"], Validators.required);
   }
 
 }
