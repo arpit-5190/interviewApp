36394de3d55504bca4efa9f6effd7161f8919b18
Arpit Rathod <arpitrathod@Arpits-MBP.home>
2020-07-30 13:15:46 -0500

    add checkbox element update to put the data into middle component

--- src/app/checkbox-element/checkbox-element.component.ts
+++ src/app/checkbox-element/checkbox-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox-element',
@@ -8,12 +8,56 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class CheckboxElementComponent implements OnInit {
 
+
   //Define Input variable which is used to get data from parent App component to child component
   @Input() dummyCheckboxData:any;
+
   
+  //Define Output variable to pass the value of question to middle component
+  @Output() public onCheckboxChange = new EventEmitter();
+
+  //Define property to store checkbox value
+  checkValue:any = {};
+
+  lastAction:string;
+  disabledStatus;
+
+  //Define an event which is called while checkbox field will change value
+  onChange(event, index, item){
+
+    item.checked = !item.checked;
+
+    if(item.value == true || item.value == "true"){
+      item.value = false;
+    }
+    else{
+      item.value = true;
+    }
+
+    //Call this function to disable none option
+    this.disableNone();
+
+    this.onCheckboxChange.emit(item);
+  }
+
+  //Define the method to enable or disable none button
+  disableNone = () => {
+    for(let option of this.dummyCheckboxData.input.selectOptions){
+      if(option.label != 'None' && (option.value == true || option.value == "true")){
+        this.dummyCheckboxData.input.selectOptions[this.dummyCheckboxData.input.selectOptions.length-1].checked = false;
+        this.dummyCheckboxData.input.selectOptions[this.dummyCheckboxData.input.selectOptions.length-1].value = "false";
+        this.dummyCheckboxData.input.selectOptions[this.dummyCheckboxData.input.selectOptions.length-1].disable = true;
+        break;
+      }
+      else{
+        this.dummyCheckboxData.input.selectOptions[this.dummyCheckboxData.input.selectOptions.length-1].disable = false;
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit() {
+    this.disableNone();
   }
 
 }
